fcb5c400356e505e1b5bd8e4c954291731915b24
Apollo <beanzilla@beanzilla.net>
2022-02-10 12:57:18 -0500

    Released v1.4.1
      Fixed possible issue with players being kicked before the mod created
    hud ids for hud elements.
    
      Also organized the settings into one place of the init rather than 2
    places.

--- init.lua
+++ init.lua
@@ -1,46 +1,12 @@
 
 inotify = {}
 --inotify.store = minetest.get_mod_storage()
+inotify.VERSION = "1.4.1"
+inotify.modpath = minetest.get_modpath("inotify")
 
 -- Settings
 inotify.interval_rate = minetest.settings:get("inotify.interval_rate")
 inotify.include_day_count = minetest.settings:get_bool("inotify.include_day_count")
-
-if inotify.interval_rate == nil then
-    inotify.interval_rate = 3.0
-    minetest.settings:set("inotify.interval_rate", 3.0)
-end
-if inotify.include_day_count == nil then
-    inotify.include_day_count = false
-    minetest.settings:set_bool("inotify.include_day_count", false)
-end
-
--- Colors, HEX, and RGB
--- Use https://duckduckgo.com/?t=ffab&q=rgb+to+hex&ia=answer to generate hex colors
-inotify.colors = {
-    white= 0xFFFFFF, -- 255, 255, 255
-    black= 0x000000, -- 0  , 0  , 0
-    green= 0x00FF00, -- 0  , 255, 0
-    dark_green = 0x00AF00, -- 0  , 175, 0
-    red= 0xFF0000, -- 255, 0  , 0
-    blue= 0x0000FF, -- 0  , 0  , 255
-    yellow= 0xFFFF00, -- 255, 255, 0
-    cyan= 0x00FFFF, -- 0  , 255, 255
-    magenta= 0xFF00FF, -- 255, 0  , 255
-    orange = 0xFF9600, -- 255, 150, 0
-}
-
-local modpath = minetest.get_modpath("inotify")
-
--- Add utility functions
-dofile(modpath.."/utils.lua")
-
--- Send off to sub-modules
-dofile(modpath.."/pos.lua")
-dofile(modpath.."/free_space.lua")
-dofile(modpath.."/health.lua")
-dofile(modpath.."/world_time.lua")
-
 local do_health = minetest.settings:get_bool("inotify.display_health")
 local do_pos = minetest.settings:get_bool("inotify.display_pos")
 local do_freespace = minetest.settings:get_bool("inotify.display_freespace")
@@ -62,6 +28,41 @@ if do_clock == nil then
     do_clock = true
     minetest.settings:set_bool("inotify.display_time", true)
 end
+
+if inotify.interval_rate == nil then
+    inotify.interval_rate = 3.0
+    minetest.settings:set("inotify.interval_rate", 3.0)
+else
+    inotify.interval_rate = tonumber(inotify.interval_rate)
+end
+if inotify.include_day_count == nil then
+    inotify.include_day_count = false
+    minetest.settings:set_bool("inotify.include_day_count", false)
+end
+
+-- Colors, HEX, and RGB
+-- Use https://duckduckgo.com/?t=ffab&q=rgb+to+hex&ia=answer to generate hex colors
+inotify.colors = {
+    white= 0xFFFFFF, -- 255, 255, 255
+    black= 0x000000, -- 0  , 0  , 0
+    green= 0x00FF00, -- 0  , 255, 0
+    dark_green = 0x00AF00, -- 0  , 175, 0
+    red= 0xFF0000, -- 255, 0  , 0
+    blue= 0x0000FF, -- 0  , 0  , 255
+    yellow= 0xFFFF00, -- 255, 255, 0
+    cyan= 0x00FFFF, -- 0  , 255, 255
+    magenta= 0xFF00FF, -- 255, 0  , 255
+    orange = 0xFF9600, -- 255, 150, 0
+}
+
+-- Add utility functions
+dofile(inotify.modpath.."/utils.lua")
+
+-- Send off to sub-modules
+dofile(inotify.modpath.."/pos.lua")
+dofile(inotify.modpath.."/free_space.lua")
+dofile(inotify.modpath.."/health.lua")
+dofile(inotify.modpath.."/world_time.lua")
 
 local interval = 0
 minetest.register_globalstep(function(dtime)
@@ -119,24 +120,23 @@ minetest.register_on_leaveplayer(function(object, timed_out)
     local pname = object:get_player_name()
     if inotify.health[pname] ~= nil then
         inotify.health[pname] = nil
-        object:hud_remove(inotify.health_hud[pname])
         inotify.health_hud[pname] = nil
     end
 
     if inotify.player_pos[pname] ~= nil then
         inotify.player_pos[pname] = nil
-        object:hud_remove(inotify.player_pos_hud[pname])
         inotify.player_pos_hud[pname] = nil
     end
 
     if inotify.free_space[pname] ~= nil then
         inotify.free_space[pname] = nil
-        object:hud_remove(inotify.free_space_hud[pname])
         inotify.free_space_hud[pname] = nil
     end
 
     if inotify.worldt_hud[pname] ~= nil then
-        object:hud_remove(inotify.worldt_hud[pname])
         inotify.worldt_hud[pname] = nil
     end
 end)
+
+minetest.log("action", "[iNotify] Version: "..inotify.VERSION)
+minetest.log("action", "[iNotify] Ready")
